467a824731c38e5b59d2eb68c8276a8e75fd4340
Karoline <boothwhack@users.noreply.github.com>
2024-12-13 16:39:29 +0100

    Re-enable position test

--- GraphVizard.Tests/NodeProperties.cs
+++ GraphVizard.Tests/NodeProperties.cs
@@ -2,32 +2,39 @@ namespace GraphVizard.Tests;
 
 public class NodeProperties
 {
-    /*[Fact]
+    [Fact]
     public void NodePositionProperty()
     {
         var graph = RootGraph.Directed("Simple Graph");
 
         var a = graph.GetNode("a");
         var b = graph.GetNode("b");
+        var c = graph.GetNode("c");
 
         a.AddEdgeTo(b);
+        a.AddEdgeTo(c);
         
         graph.Layout("dot");
         
         /*
          * Should produce graph roughly:
-         * |---|
-         * | a |
-         * |---|
-         *   |
-         * |---|
-         * | b |
-         * |---|
+         *    |---|
+         *    | a |
+         *    |---|
+         *    /   \
+         * |---| |---|
+         * | b | | c |
+         * |---| |---|
          */
         
-        /*Assert.Equal(a.Position.X, b.Position.X);
-        Assert.True(a.Position.Y > b.Position.Y);
-    }*/
+        Assert.True(b.Position.X < a.Position.X);
+        Assert.True(a.Position.X < c.Position.X);
+        Assert.True(b.Position.X < c.Position.X);
+
+        Assert.Equal(b.Position.Y, c.Position.Y);
+        Assert.True(b.Position.Y < a.Position.Y);
+        Assert.True(c.Position.Y < a.Position.Y);
+    }
 
     [Fact]
     public void NodeNameProperty()
